a790d4f84df4f8eb6643789a4ef09a95f4821075
Owner <Owner@ANT.dhcp.wustl.edu>
2022-09-08 14:04:01 -0500

    Studio 1 done

---
+++ src/studio1/Average.java
@@ -0,0 +1,22 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("The first number to be averaged?");
+		int n1 = in.nextInt();
+		System.out.println("The second number to be averaged?");
+		int n2 = in.nextInt();
+		double avg = (n1 + n2)/2.0;
+		System.out.println("Average of " + n1 + " and " + n2 + " is " + avg);
+		
+		
+		
+		
+	}
+
+}
